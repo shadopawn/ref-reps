428482ab1dba1b65fa657684e2e68b133d1b736d
Daniel Litt <daniel_litt@comcast.net>
2020-11-20 15:45:27 -0500

    Extract file download method to own class

---
+++ ref-reps-unity-project/Assets/Scripts/Database/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Database : MonoBehaviour
+{
+    public void DownloadFile(String path)
+    {
+        Firebase.Storage.FirebaseStorage storage = Firebase.Storage.FirebaseStorage.DefaultInstance;
+        Firebase.Storage.StorageReference
+            reference = storage.GetReference(path);
+
+        String fileName = Path.GetFileName(path);
+        Debug.Log(fileName);
+
+        string local_url = Application.dataPath +"/VideoFiles/"+ fileName;
+        Debug.Log(local_url);
+
+        // Download to the local filesystem
+        reference.GetFileAsync(local_url).ContinueWith(task =>
+        {
+            if (!task.IsFaulted && !task.IsCanceled)
+            {
+                Debug.Log("File downloaded.");
+            }
+            else
+            {
+                Debug.LogError("File failed to download");
+            }
+        });
+    }
+}
