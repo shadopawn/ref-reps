eedb66658ae21a88e193ada20aac0784b40a8553
Daniel Litt <daniel_litt@comcast.net>
2021-01-28 10:11:13 -0500

    Add JsonParser and lesson pack button prefab

---
+++ ref-reps-unity-project/Assets/Scripts/Helpers/JsonParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+public class JsonParser
+{
+
+    public List<String> GetLessonPackNames(String json)
+    {
+        JObject lessonPacks = JObject.Parse(json);
+
+        List<String> lessonPackNames = new List<string>();
+        foreach (KeyValuePair<string, JToken> lessonPack in lessonPacks)
+        {
+            lessonPackNames.Add(lessonPack.Value["name"]?.ToString());
+            JToken lessonPairs = lessonPack.Value["lesson_pairs"];
+        }
+
+        return lessonPackNames;
+    }
+}
