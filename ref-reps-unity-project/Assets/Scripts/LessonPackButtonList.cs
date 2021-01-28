f48497d352d672439a1dfa3fb67b4dbb9416d2d6
Daniel Litt <daniel_litt@comcast.net>
2021-01-28 10:32:44 -0500

    Add lesson pack buttons based on database

---
+++ ref-reps-unity-project/Assets/Scripts/LessonPackButtonList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LessonPackButtonList : MonoBehaviour
+{
+
+    public GameObject lessonPackButtonPrefab;
+    
+    private Database _database = new Database();
+    private JsonParser _jsonParser = new JsonParser();
+
+    async void Start()
+    {
+        String json = await _database.GetLessonPacksJson();
+        List<String> lessonPackNames = _jsonParser.GetLessonPackNames(json);
+
+        foreach (var lessonPackName in lessonPackNames)
+        {
+            print(lessonPackName);
+            CreateNewButton(lessonPackName);
+        }
+    }
+
+    private void CreateNewButton(String buttonText)
+    {
+        GameObject newButton = Instantiate(lessonPackButtonPrefab, transform);
+        Text buttonTextComponent = newButton.GetComponentInChildren<Text>();
+        buttonTextComponent.text = buttonText;
+    }
+}
