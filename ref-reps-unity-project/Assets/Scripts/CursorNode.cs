05c3ca02aea08af27913c145cd84661eb4f231d4
Daniel Litt <daniel_litt@comcast.net>
2021-01-31 17:23:21 -0500

    Fix slide button activation

---
+++ ref-reps-unity-project/Assets/Scripts/CursorNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorNode : MonoBehaviour
+{
+    public GameObject parentButton;
+    
+    // Start is called before the first frame update
+    public void titleExecute()
+    {
+        parentButton.GetComponent<LessonSelectScript>()?.titleExecute();
+    }
+}
