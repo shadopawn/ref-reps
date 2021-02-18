356e2f8a20e156dae135154a1f102f4012ef95b2
Daniel Litt <daniel_litt@comcast.net>
2021-02-17 20:52:01 -0500

    create initial saveData class

---
+++ ref-reps-unity-project/Assets/Scripts/Helpers/SaveData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class SaveData
+{
+    private string saveFile = Application.dataPath + "/SaveData/userInfo.json";
+    private string saveDivectory = Application.dataPath + "/SaveData";
+
+    public SaveData()
+    {
+        FileInfo fileInfo = new FileInfo(saveDivectory);
+        if (!fileInfo.Directory.Exists) fileInfo.Directory.Create();
+    }
+    
+    public void CompleteLessonPair(string lessonPackName, string lessonPairName)
+    {
+        
+    }
+}
---
+++ ref-reps-unity-project/Assets/Tests/EditMode/SaveDataTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Newtonsoft.Json.Linq;
+
+namespace Tests
+{
+    public class SaveDataTests 
+    {
+        private SaveData _saveData;
+
+        [SetUp]
+        public void Setup()
+        {
+            _saveData = new SaveData();
+        }
+
+        [Test]
+        public void TestCompleteLessonPair()
+        {
+            _saveData.CompleteLessonPair("", "");
+            Assert.NotNull("");
+        }
+    }
+}
