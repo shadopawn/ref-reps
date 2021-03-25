5a5893e861591d38a2b42da42f0ae0f740c4eef8
Daniel Litt <daniel_litt@comcast.net>
2021-03-24 21:48:17 -0400

    Add inital unity analytics custom events

--- ref-reps-unity-project/Assets/Scripts/LessonModuleController.cs
+++ ref-reps-unity-project/Assets/Scripts/LessonModuleController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class LessonModuleController : MonoBehaviour
 {
@@ -47,21 +48,38 @@ public class LessonModuleController : MonoBehaviour
 
     public void SaveCompleteCurrentLessonPair()
     {
+        if (_saveData.IsLessonPairComplete(lessonPackName, GetCurrentLessonPairName()) == false)
+        {
+            CustomAnalyticEvent("CompleteLessonPair");
+        }
+        
         _saveData.CompleteLessonPair(lessonPackName, GetCurrentLessonPairName());
     }
 
     public void SaveMakeCorrectCall()
     {
         _saveData.MakeCorrectCall(lessonPackName, GetCurrentLessonPairName());
+        CustomAnalyticEvent("MakeCorrectCall");
     }
     
     public void SaveMakeIncorrectCall()
     {
         _saveData.MakeIncorrectCall(lessonPackName, GetCurrentLessonPairName());
+        CustomAnalyticEvent("MakeIncorrectCall");
     }
 
     public void SaveAnalysisView()
     {
         _saveData.AddAnalysisView(lessonPackName, GetCurrentLessonPairName());
+        CustomAnalyticEvent("AnalysisVideoViewed");
+    }
+
+    private AnalyticsResult CustomAnalyticEvent(String eventName)
+    {
+        return Analytics.CustomEvent(eventName, new Dictionary<string, object>
+        {
+            { "lessonPackName", lessonPackName },
+            { "lessonPairName", GetCurrentLessonPairName() }
+        });
     }
 }
