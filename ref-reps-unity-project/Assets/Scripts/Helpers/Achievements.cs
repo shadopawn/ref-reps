71a4c8304a5f54b1696e3a3ac21e289645d74ed2
Daniel Litt <daniel_litt@comcast.net>
2021-03-29 20:53:13 -0400

    Add start for achievement system

---
+++ ref-reps-unity-project/Assets/Scripts/Helpers/Achievements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+public class Achievements
+{
+    
+    private JObject _AchievementsJObject = new JObject();
+
+    public Achievements(String achievementsFileName = null)
+    {
+        achievementsFileName = achievementsFileName ?? "Achievements";
+        TextAsset achievementsTextAsset = Resources.Load<TextAsset>(achievementsFileName);
+        if (achievementsTextAsset == null)
+        {
+            Debug.LogError("Achievements file not found");
+        }
+        else
+        {
+            _AchievementsJObject = JObject.Parse(achievementsTextAsset.text);
+        }
+    }
+
+    public void CompleteAchievement(String achievementName)
+    {
+        
+    }
+    
+    public List<(String name, String description, bool completed)> GetAchievements()
+    {
+        return new List<(string name, string description, bool completed)>();
+    }
+}
---
+++ ref-reps-unity-project/Assets/Tests/EditMode/AchievementsTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Newtonsoft.Json.Linq;
+using UnityEngine.SocialPlatforms.Impl;
+
+namespace Tests
+{
+    public class AchievementsTests 
+    {
+        private Achievements _achievements;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _achievements = new Achievements("TestAchievements");
+        }
+        
+        [Test]
+        public void TestCreateAchievementObject()
+        {
+            Assert.NotNull(_achievements);
+        }
+        
+        [Test]
+        public void TestNonExistentAchievementFile()
+        {
+            _achievements = new Achievements("NonExistentAchievementFile");
+            LogAssert.Expect(LogType.Error, "Achievements file not found");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
+
